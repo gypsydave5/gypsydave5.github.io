cbe3a990637ff1d7d936b7c5c56c88d4ae393ac2
David Wickes <dave.wickes@gmail.com>
2018-02-11 20:57:51 +0000

    publish script

--- publish.sh
+++ publish.sh
@@ -1,13 +1,4 @@
-# system "git init"
-# system "git add ."
-# message = "Site updated at #{Time.now.utc}"
-# system "git commit -m #{message.inspect}"
-# system "git remote add origin git@github.com:#{GITHUB_REPONAME}.git"
-# system "git push origin master --force"
-
 TEMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
-
-echo $TEMPDIR
 
 cp -r site/. $TEMPDIR
 pushd $TEMPDIR
@@ -15,10 +6,10 @@ pushd $TEMPDIR
 git init
 git add .
 git status
-git commit -m Updating site from blawg
+git commit -m "Updating site from blawg at $(date)"
 git remote add origin git@github.com:gypsydave5/gypsydave5.github.io
-# git push origin master --force
+git push origin master --force
 
-# popd $TEMPDIR
-# rm -rf $TEMPDIR
+popd $TEMPDIR
+rm -rf $TEMPDIR
 
