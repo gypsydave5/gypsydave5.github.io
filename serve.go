327397f90333c8b9179422d7500bfd41fb1d3d81
David Wickes <dave.wickes@gmail.com>
2019-03-08 18:08:57 +0000

    better serving

--- serve.go
+++ serve.go
@@ -9,6 +9,6 @@ func main() {
 	fs := http.FileServer(http.Dir("site"))
 	http.Handle("/", fs)
 
-	log.Println("Serving site on port 8000")
+	log.Println("Serving site on port 8000 (http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
