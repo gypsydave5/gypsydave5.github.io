52d6647581eb759556544cf1b775099caf4d3c71
David Wickes <dave.wickes@gmail.com>
2021-02-27 11:14:06 +0000

    publjish

--- publish.sh
+++ publish.sh
@@ -12,6 +12,7 @@ git status
 git commit -m "Updating site from blawg at $(date)"
 git remote add origin git@github.com:gypsydave5/gypsydave5.github.io
 git push origin master --force
+aws s3 sync . s3://blog.gypsydave5.com --profile=gypsydave5
 
 popd $TEMPDIR
 rm -rf $TEMPDIR
